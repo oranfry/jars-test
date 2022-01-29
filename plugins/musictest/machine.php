bb822eadf1bdc2c1da89e3ece03e0dd1c7085fd1
Oran Fry <oran@emerg.co>
2022-01-29 17:02:25 +1300

    Verbose
    Accept verbose flag to test command and honour.

--- plugins/musictest/machine.php
+++ plugins/musictest/machine.php
@@ -2,7 +2,6 @@
 
 const USERNAME = 'music';
 const PASSWORD = '983dk32dfmfa1s';
-const VERBOSE = 1;
 
 define('TEST_HOME', dirname(dirname(__DIR__)));
 define('SAVE_CMD', TEST_HOME . '/portals/music/cli.php -u ' . USERNAME . ' -p ' . PASSWORD . ' save');
--- plugins/musictest/src/php/class/musictest/Router.php
+++ plugins/musictest/src/php/class/musictest/Router.php
@@ -5,6 +5,7 @@ namespace musictest;
 class Router extends \Router
 {
     protected static $routes = [
-        'CLI test' => ['PAGE', 'LAYOUT' => 'raw'],
+        'CLI test' => ['PAGE', 'VERBOSE' => false, 'LAYOUT' => 'raw'],
+        'CLI test -v' => ['PAGE', null, 'VERBOSE' => true, 'LAYOUT' => 'raw'],
     ];
 }
