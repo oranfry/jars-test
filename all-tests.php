1555b79cb08bc99bc2f2e12cde4d67c704020ac3
Oran Fry <oran@emerg.co>
2022-04-25 10:48:54 +1200

    Tests
    Renumber tests as to group them and allow more items in the same group in the future without further renumbering.

--- all-tests.php
+++ all-tests.php
@@ -1,15 +1,15 @@
 <?php
 
-do_change('001-token'); // creates a token, but it is not actually used during tests
+do_change('010-token'); // creates a token, but it is not actually used during tests
 
-do_change_and_test('002-artists-and-albums', 'check-albums-and-artists');
-do_change_and_test('003-more-artists-and-albums', 'check-albums-and-artists');
-do_change_and_test('004-delete', 'check-albums-and-artists');
-do_change_and_test('005-add-tracks', 'check-album-tracks');
-do_change_and_test('006-images', 'verify-images');
-do_change_and_test('007-external-ids', 'check-external-ids');
-do_change_and_test('008-preview', 'check-album-tracks');
-do_change_and_test('009-timestamps-a', 'check-release-dates');
-do_change_and_test('010-timestamps-b', 'check-release-dates');
-do_change_and_test('011-inline-child-propagation-forward', 'check-downloads');
-do_change_and_test('012-inline-child-propagation-back', 'check-downloads');
+do_change_and_test('020-artists-and-albums', 'check-albums-and-artists');
+do_change_and_test('021-more-artists-and-albums', 'check-albums-and-artists');
+do_change_and_test('030-delete', 'check-albums-and-artists');
+do_change_and_test('040-add-tracks', 'check-album-tracks');
+do_change_and_test('050-images', 'verify-images');
+do_change_and_test('060-external-ids', 'check-external-ids');
+do_change_and_test('070-preview', 'check-album-tracks');
+do_change_and_test('080-timestamps-a', 'check-release-dates');
+do_change_and_test('081-timestamps-b', 'check-release-dates');
+do_change_and_test('090-inline-child-propagation-forward', 'check-downloads');
+do_change_and_test('091-inline-child-propagation-back', 'check-downloads');
